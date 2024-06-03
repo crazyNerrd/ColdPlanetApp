6809e4e63d4e6c9be1993ea5f0631f27b328b82b
crazyNerrd <mitesh.ramteke2020@vitstudent.ac.in>
2024-06-04 00:45:08 +0530

    BUG: fixed the background color issue for chrome and safari

--- theme.js
+++ theme.js
@@ -11,7 +11,7 @@ const styles = {
     body: {
       bg: mode(
         props.theme.semanticTokens.colors["chakra-body-bg"]._light,
-        "blackAlpha.900"
+        "white"
       )(props),
     },
   }),
